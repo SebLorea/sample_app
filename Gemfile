f8d39ae40e889aeab159d31344037b077bf98c91
Sebastien Lorea <slo@intotheweb.be>
2013-04-23 09:35:46 +0200

    Modeled Users

--- Gemfile
+++ Gemfile
@@ -9,11 +9,11 @@ gem 'sqlite3'
 
 
 group :development do
-  gem 'rspec-rails', '2.5.0'
+  gem 'rspec-rails', '2.6.1.beta1'
 end
 
 group :test do
-  gem 'rspec', '2.5.0'
+  gem 'rspec', '2.6'
   gem 'webrat', '0.7.1'
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	def titre   #ceci est une méthode
+		base_titre = "Titre sample App"
+		if @titre.nil?
+			base_titre
+		else
+
+			base_titre + " " + @titre
+			#{}"#{base_titre} | #{@titre}"
+		end
+	end
+
+	def appTitle
+
+		"Sample App"
+
+	end
 end
